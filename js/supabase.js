a5a655ca9b45e542c039bfedce8e1faf094cc550
TheArian20 <146113948+TheArian20@users.noreply.github.com>
2026-08-05 13:41:29 -0500

    fix: usar sesiones temporales de acceso

--- js/supabase.js
+++ js/supabase.js
@@ -1,3 +1,10 @@
 window.STESIN_SUPABASE_URL = "https://sllalwueqogysuovsntx.supabase.co";
 window.STESIN_SUPABASE_KEY = "sb_publishable_LU7fTFxErYUrmzoq6Fxl-w_g08a7n4W";
-window.STESIN_SUPABASE = window.supabase.createClient(window.STESIN_SUPABASE_URL, window.STESIN_SUPABASE_KEY);
+window.STESIN_SUPABASE = window.supabase.createClient(window.STESIN_SUPABASE_URL, window.STESIN_SUPABASE_KEY, {
+    auth: {
+        storage: window.sessionStorage,
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true
+    }
+});
