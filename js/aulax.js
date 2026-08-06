f5052c26e1e5c440846902a802878e3917e87d7d
TheArian20 <146113948+TheArian20@users.noreply.github.com>
2026-08-06 11:57:27 -0500

    Ocultar enlaces de asistencia y equipo

--- js/aulax.js
+++ js/aulax.js
@@ -564,7 +564,6 @@ const AulaX = (() => {
         agregarEnlaceSilabos();
         agregarEnlaceAlabanzas();
         agregarEnlaceRedes();
-        agregarEnlacesComunidad();
         agregarEnlaceAdministracion();
         configurarMenuMovil();
         configurarModoOscuroGlobal();
