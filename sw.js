30fc61f5cc9ccdc194c574ec8494c0b9895b8dd9
TheArian20 <146113948+TheArian20@users.noreply.github.com>
2026-09-18 10:44:09 -0500

    Simplificar la sección de Contacto

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "stesin-v26";
+const CACHE = "stesin-v27";
 const BASE = ["./", "index.html", "404.html", "login.html", "contacto.html", "biblioteca.html", "visor.html", "calendario.html", "silabos.html", "css/style.css", "css/login.css", "js/aulax.js", "js/dashboard.js", "js/biblioteca.js", "js/visor.js", "js/agenda.js", "js/silabos.js", "js/firebase-inicio.js", "js/firebase.js", "js/firebase-datos.js", "js/visitas.js", "biblioteca-catalogo.js", "biblioteca-enlaces.js", "biblioteca-adicionales.js", "assets/equipo/oiser-ramos-nunez.jpg", "assets/equipo/javier-edilberto-vasquez-vasquez.jpeg", "manifest.json", "stesin-app-icon-v2-192.png", "stesin-app-icon-v2-512.png", "stesin-app-maskable-v2-192.png", "stesin-app-maskable-v2-512.png"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(BASE)).then(() => self.skipWaiting())));
